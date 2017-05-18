9e3410cb18aaf56afb43845f79094bbdea6a3e8e
Vladimir Novick <vlad.novick@gmail.com>
2017-05-18 14:47:25 +0300

    Copiright

--- JsonServicesTester/Properties/AssemblyInfo.cs
+++ JsonServicesTester/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BeproDB_Test")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("JsonServerTester")]
+[assembly: AssemblyDescription(" Test System for Micro Services")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BeproDB_Test")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("JsonServerTester")]
+[assembly: AssemblyCopyright("Copyright  © Vladimir Novick  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
